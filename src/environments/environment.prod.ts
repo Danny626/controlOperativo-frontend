d07b68f34b48ed871eb2ce7aadcca5833e4348e0
Danny <live626@gmail.com>
2022-02-10 15:27:05 -0400

    env removed

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  HOST: 'http://${HOST}/controlOperativo-backend',
-  PATH_ROOT: '/controlOperativo',
-  REINTENTOS: 3,
-  TOKEN_AUTH_USERNAME: 'controlOperativo',
-  TOKEN_AUTH_PASSWORD: '${TOKEN_AUTH_PASSWORD}',
-  TOKEN_NAME: 'access_token'
-};
